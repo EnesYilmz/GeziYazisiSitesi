2bc2b5316c5b24db9233ec1b3179793b30dcafdb
Enes Yılmaz <39797616+EnesYilmz@users.noreply.github.com>
2019-01-28 22:16:05 +0300

    Projeye üye kısmı eklendi.

---
+++ Core2Identity/Models/ApplicationIdentityDbContext.cs
@@ -0,0 +1,27 @@
+﻿using GeziYazisiSitesi.Modals;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2Identity.Models
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Yazi> Yazi { get; set; }
+
+        public DbSet<Ulke> Ulke { get; set; }
+
+        public DbSet<Sehir> Sehir { get; set; }
+
+    }
+
+}
---
+++ Core2Identity/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2Identity.Models
+{
+    //IdentityUser---temel bir sınıf içinde kimlik ile ilgili veriler vardır
+    public class ApplicationUser:IdentityUser
+    {
+
+    }
+}
